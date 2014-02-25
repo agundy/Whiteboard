22f281959e1ec71c58ecb5899891e59125b387b0
Burke Livingston <burkel24@gmail.com>
2014-02-25 16:10:16 -0500

    Really fixed SITE_ROOT

--- trackSchool/trackSchool/settings.py
+++ trackSchool/trackSchool/settings.py
@@ -8,7 +8,7 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-SITE_ROOT = os.path.join("..", os.path.dirname(os.path.realpath(__file__)))
+SITE_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..",)
 
 
 MANAGERS = ADMINS
