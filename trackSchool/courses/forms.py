c79a95219aa08855ab4779eea9e873c7c9e13de2
Burke Livingston <burkel24@gmail.com>
2014-03-03 15:22:05 -0500

    All create student fields now required

--- trackSchool/courses/forms.py
+++ trackSchool/courses/forms.py
@@ -7,6 +7,11 @@ class StudentForm(forms.ModelForm):
         fields = ('email', 'first_name', 'last_name', 'username', 'password')
         widgets = {'password': forms.PasswordInput()}
 
+    def __init__(self, *args, **kwargs):
+        super(StudentForm, self).__init__(*args, **kwargs)
+        for key in self.fields:
+            self.fields[key].required = True
+
 class LoginForm(forms.Form):
   class Meta:
     model = User
