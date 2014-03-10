df5a35a73b5f5684f32eca46642bd134e767350b
Aaron <airgunde@gmail.com>
2014-03-09 23:38:01 -0400

    Fixed create group permissions for render

--- trackSchool/gradeGroup/views.py
+++ trackSchool/gradeGroup/views.py
@@ -56,7 +56,7 @@ def create_grade_group(request):
 
             errors = ['Error: Invalid input']
 
-            return render_to_response('Group/create_group.html', {'form': clean_form, 'errors': errors})
+            return render_to_response('Group/create_group.html', {'form': clean_form, 'errors': errors},RequestContext(request))
 
     else:
 
