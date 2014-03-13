6865c36bea6be915cd4ca2e195bc3a2ddb9b2b38
Aaron <airgunde@gmail.com>
2014-03-13 02:10:39 -0400

    Better admin page

--- trackSchool/courses/admin.py
+++ trackSchool/courses/admin.py
@@ -2,10 +2,15 @@ from models import *
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ('title', 'dept', 'courseID')
+		
+
 for model in (
     School,
     Student,
-    Course,
+    # Course,
 ):
     admin.site.register(model)
 
+admin.site.register(Course, CourseAdmin)
