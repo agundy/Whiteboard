192c5ac7106a022df739a367d83387182613ac8d
Aaron <airgunde@gmail.com>
2014-09-11 17:28:24 -0400

    Ran a linter on all python files and made them conform with PEP8

--- trackSchool/courses/urls.py
+++ trackSchool/courses/urls.py
@@ -2,24 +2,25 @@ from django.conf.urls import patterns, include, url
 from courses.views import *
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-
+urlpatterns = patterns(
+    '',
     # Student
     url(r'^student/create', Student.create_student),
     url(r'^student/join_school/$', Student.join_school),
     url(r'^student/dashboard', Student.show_dashboard),
-    url(r'^confirm_email/(?P<confirmation_code>\w{0,50})/(?P<username>\w{0,50})/$',Student.confirm_edu_email),
+    url(r'^confirm_email/(?P<confirmation_code>\w{0,50})/(?P<username>\w{0,50})/$',
+        Student.confirm_edu_email),
     url(r'^accounts/login', Student.login),
     url(r'^accounts/logout', Student.logout),
     url(r'^student/profile/(\d+)$', Student.profile),
     url(r'^student/profile/$', Student.profile),
-    url(r'^student/groups/',Student.show_student_groups),
+    url(r'^student/groups/', Student.show_student_groups),
     url(r'^student/grades/', Student.show_grades),
     url(r'^student/js_grades/', Student.js_grades),
     url(r'^student/edit/', Student.student_edit),
-    url(r'^student/assignment/add/(\d+)$',Student.add_student_item),
-    url(r'^student/assignment/remove/(\d+)$',Student.remove_student_item),
-    url(r'^student/assignment/edit/(\d+)$',Student.edit_student_item),
+    url(r'^student/assignment/add/(\d+)$', Student.add_student_item),
+    url(r'^student/assignment/remove/(\d+)$', Student.remove_student_item),
+    url(r'^student/assignment/edit/(\d+)$', Student.edit_student_item),
     url(r'^student/assignment_type/add/(\d+)$', Student.assignment_type_add),
     url(r'^student/assignment_type/edit/(\d+)$', Student.assignment_type_edit),
     url(r'^student/forgot-password', Student.forgot_password),
