74c6ac5b80efa903599bf929fb2625cbe9ada3d9
Aaron Gunderson <aaron@agundy.com>
2014-10-12 15:12:03 -0400

    Updated grade graphs to use d3.js

--- trackSchool/static/js/grade_graph.js
+++ trackSchool/static/js/grade_graph.js
@@ -1,55 +1,99 @@
-var graph_width = 200;
-var graph_height = 200;
-var bar_height = 175;
-var bar_width = 35;
-var bar_padding = 25;
-var padding = 0;
-var canvas;
-var grades;
 var GREEN = '#0AEA0F';
 var YELLOW_GREEN = '#C5EA0A';
 var YELLOW = '#E7EA0A';
 var ORANGE = '#FFA000';
 var RED = '#EA200A';
 
-var init = function(){
-    canvas = document.getElementById("canvas");
-    canvas.width = graph_width;
-    canvas.height = graph_height;
-    context = canvas.getContext("2d");
-    initDom();
-    graph(grades);
-}
+var grades;
 
-function initDom(){
-    $(canvas).css({"display":"block"});
-};
-
-
-var graph = function(grades){
-    context.clearRect(0,0,graph_width,graph_height);
-    context.font = '13px';
-    context.textBaseline = 'top';
-    for(var i = 0; i < grades.length; i++){
-        if (grades[i]['grade'] >= 90) {
-            context.fillStyle = GREEN;
-        } else if(grades[i]['grade'] >= 80){
-            context.fillStyle = YELLOW_GREEN;
-        } else if(grades[i]['grade'] >= 70){
-            context.fillStyle = YELLOW;
-        } else if(grades[i]['grade'] >= 60){
-            context.fillStyle = ORANGE;
-        } else if(grades[i]['grade'] < 60){
-            context.fillStyle = RED;
-        };
-        context.fillRect(i*bar_padding +i*bar_width + padding,bar_height*(1-(grades[i]['grade']/100)),bar_width,bar_height*(grades[i]['grade'])/100)
-        context.fillStyle = '#000';
-        context.fillText(grades[i]['course'],i*bar_padding+ i*bar_width+padding,graph_height-10);
-        console.log(grades[i]);
-    }
-}
-
-$.get("/student/js_grades/",{request:'{{ csrf_token }}'}).done(function( data ) {
+$.get("/student/js_grades/",{request:''}).done(function( data ) {
+    // Get grades information from ajax request
     grades = data;
-    init();
+    InitChart();
 });
+
+
+function InitChart() {
+    var barData = [];
+    
+    for(var i = 0; i < grades.length; i++){
+        barData.push({
+            'x': grades[i]['course'],
+            'y': grades[i]['grade'],
+            'i': i
+        })
+    };
+    console.log("passed");
+
+
+    var vis = d3.select('#visualisation'),
+    WIDTH = 325,
+    HEIGHT = 300,
+    MARGINS = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 50
+    },
+    xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function (d) {
+      return d.x;
+    })),
+
+
+    yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
+        d3.max([100,      
+            d3.max(barData, function (d) {
+                return d.y;
+              })
+            ])
+    ]),
+    xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true),
+
+    yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient("left")
+      .tickSubdivide(true);
+
+
+    vis.append('svg:g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+    .call(xAxis);
+
+    vis.append('svg:g')
+    .attr('class', 'y axis')
+    .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+    .call(yAxis);
+
+    vis.selectAll('rect')
+    .data(barData)
+    .enter()
+    .append('rect')
+    .attr('x', function (d) {
+      return xRange(d.x);
+    })
+    .attr('y', function (d) {
+      return yRange(d.y);
+    })
+    .attr('width', xRange.rangeBand())
+    .attr('height', function (d) {
+      return ((HEIGHT - MARGINS.bottom) - yRange(d.y));
+    })
+    .attr('fill', function(d) {
+        if (d.y >= 90){
+            return GREEN;
+        } else if(d.y >= 80){
+            return YELLOW_GREEN;
+        } else if(d.y >= 70){
+            return YELLOW;
+        } else if(d.y >= 60){
+            return ORANGE;
+        } else if(d.y < 60){
+            return RED;
+        }
+    });
+}
