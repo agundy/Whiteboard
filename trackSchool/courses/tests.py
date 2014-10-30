4b93a3775ea70c69acc9e2d88ad2223695c343cd
Aaron Gunderson <aaron@agundy.com>
2014-10-30 15:08:44 -0400

    Reverted course tests to working state

--- trackSchool/courses/tests.py
+++ trackSchool/courses/tests.py
@@ -1,16 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from courses.models import Student
-from models import Student, CourseItem
 
 
 class CoursesViewsTestCases(TestCase):
 
-<<<<<<< HEAD
-def test_student_models(self):
-    """
-    test if a student was created properly
-    """
+    """docstring for CoursesViewsTestCases"""
+
     def setUp(self):
         user = User.objects.create_user(
             'temporary', 'temporary@email.com', 'temporary')
@@ -21,18 +17,7 @@ def test_student_models(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
 
-
     def test_dashboard(self):
         self.client.login(username='temporary@gmail.com', password='temporary')
         resp = self.client.get('/student/dashboard', follow=True)
         self.assertEqual(resp.status_code, 200)
-
-
-def create_courseitem(self):
-    """
-    test creation of a CourseObject
-    """
-    course_object = CourseItem(type="exam", name="exam 1")
-    course_object.due_date = "4/7/2018"
-    course_object.save()
-    assert get_object_or_404(CourseItem, name="exam 1")
